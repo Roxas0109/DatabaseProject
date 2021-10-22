39fc7fe4a2d4d7f11580fd0ebe08f9bd6a666498
Alan <alanhp2@hotmail.com>
2021-10-21 18:22:53 -0700

    login syling done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Login from './components/Login';
+import logo  from './ECSlogo.jpg';
 
 function App() {
   return (
     <div className="App">
+      <img src={logo} className="ECSlogo"/>
       <Login/>
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,16 +2,28 @@ import React from 'react'
 import './Login.css'
 
 export default function Login() {
+    const handleSubmit = ()=>{
+        console.log("Hello")
+    }
+
     return (
         <div className = "loginContainer">
-            <h1>Log In</h1>
-            <br />
             <div className="wrapper">
-                <label for="Username">Username</label>
-                <input placeholder="Username..." id="Username"></input>
+                <h1>Log In</h1>
                 <br />
-                <label for="Password">Password</label>
-                <input placeholder="Password..." id="Password"></input>
+                {/*<label><b>Username</b></label>
+                <input placeholder="Username..."></input>
+                <br />
+                <label><b>Password</b></label>
+                <input placeholder="Password..."></input>
+                <br />
+                <button onClick={handleSubmit} className="submitbtn">Submit</button>*/}
+                <form>
+                    <label>
+                        <b>Username</b>
+                        <input type="text" value={state.value} onChange={handleChange}/>
+                    </label>
+                </form>
             </div>
         </div>
     )
