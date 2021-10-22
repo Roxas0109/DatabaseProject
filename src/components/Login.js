ae8acfe069254779cc9d8305732f6e1021c92656
Alan <alanhp2@hotmail.com>
2021-10-22 08:44:38 -0700

    able to receive form data

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
-import React from 'react'
 import './Login.css'
+import React, { Component } from 'react'
 
-export default function Login() {
+{/*export default function Login() {
     const handleSubmit = ()=>{
         console.log("Hello")
     }
@@ -21,4 +21,60 @@ export default function Login() {
             </div>
         </div>
     )
+}*/}
+
+export default class Login extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            username:'',
+            password:'',
+        }
+    }
+
+    // using arrow functions to avoid binding
+    handleChange = (e) =>{
+        this.setState({
+            [e.target.name]:e.target.value,
+        });
+    }
+
+    handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log(this.state);
+    }
+
+    render() {
+        return (
+            <div className = "loginContainer">
+            <div className="wrapper">
+                <h1>Log In</h1>
+                <br />
+                <form>
+                    <label><b>Username</b>
+                        <input type="text"
+                            name="username" 
+                            value={this.state.username} 
+                            onChange={this.handleChange} 
+                            placeholder="Username..."/>
+                    </label>
+                    <br />
+                    <label><b>Password</b>
+                        <input type="password" 
+                            name="password"
+                            value={this.state.password} 
+                            onChange={this.handleChange} 
+                            placeholder="Password..."/>
+                    </label>
+                    <br />
+                    <button type="submit" 
+                        onClick={this.handleSubmit}
+                        className="submitbtn">Submit</button>
+                </form>
+            </div>
+        </div>
+        )
+    }
 }
+
