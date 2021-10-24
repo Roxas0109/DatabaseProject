68fbf4ece392c984e1d56aeaae192f5552ff7f10
Alan <alanhp2@hotmail.com>
2021-10-23 19:46:35 -0700

    successfully connected to db

--- src/server/server.js
+++ src/server/server.js
@@ -3,16 +3,21 @@ const app = express();
 const mysql = (require('mysql'));
 
 const db = mysql.createPool({
-    host: 'localhost',
+    host: '127.0.0.1',
     user: 'comp440',
     password: 'pass1234',
-    database: 'dbproject',
+    database: 'dbproject'
 });
 
 app.get('/', (req, res)=>{
-    res.send('helloworld im alqn')
+    const sqlInsert = "INSERT INTO user (username, password, firstName, lastName, email) VALUES ('al', 'a', 'b', 'c', 'd')";
+    db.query(sqlInsert, (err, result)=>{
+        res.send('inserted!')
+    })
 });
 
 app.listen(3001, ()=>{
     console.log('running on port 3001')
 });
+
+//for nodemon: npm run devStart
